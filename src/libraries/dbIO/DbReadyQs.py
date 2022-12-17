e26fbab10fe02f9a7485528a07e2277824fc066c
Myron <gmyron1@gmail.com>
2022-12-17 19:36:35 +0200

    SQLite queries for points of each team

--- src/libraries/dbIO/DbReadyQs.py
+++ src/libraries/dbIO/DbReadyQs.py
@@ -8,14 +8,15 @@ class DbReadyQs:
 
     def format(cursor: object):
         "This function formats the outputs from the database for the GUI"
-        return list(cursor)
+        # return {entry[0]:entry[1] for entry in cursor}
+        return dict(cursor)
 
     def matches_for_each_team():
         cursor = DbReadyQs.db.execute("SELECT team_name, count(match_id) FROM team, participation \
             WHERE team_name=home_team OR team_name=away_team GROUP BY team_name ORDER BY team_name")
         return DbReadyQs.format(cursor)
 
-    def goals_for_each_team():
+    def scored_goals_for_each_team():
         "goals that each team scored"
         cursor = DbReadyQs.db.execute("SELECT team_name, sum(goals) FROM \
             (SELECT team_name, sum(home_team_goals) AS goals FROM team, participation, match \
@@ -42,6 +43,27 @@ class DbReadyQs:
             GROUP BY team_name ORDER BY count(match.match_id) DESC")
         return DbReadyQs.format(cursor)
 
+    def ties_for_each_team():
+        cursor = DbReadyQs.db.execute("SELECT team_name, count(match.match_id) FROM team, participation, match \
+            WHERE participation.match_id=match.match_id AND \
+            (team_name=home_team AND home_team_goals=away_team_goals) \
+            GROUP BY team_name ORDER BY count(match.match_id) DESC")
+        return DbReadyQs.format(cursor)
+
+    def defeats_for_each_team():
+        cursor = DbReadyQs.db.execute("SELECT team_name, count(match.match_id) FROM team, participation, match \
+            WHERE participation.match_id=match.match_id AND \
+            ((team_name=home_team AND home_team_goals<away_team_goals) OR (team_name=away_team AND home_team_goals>away_team_goals)) \
+            GROUP BY team_name ORDER BY count(match.match_id) DESC")
+        return DbReadyQs.format(cursor)
+    
+    def points_for_each_team():
+        wins, ties = DbReadyQs.wins_for_each_team(), DbReadyQs.ties_for_each_team()
+        points = lambda team: 3*wins[team] + 1*ties[team]
+        return {team:points(team) for team in ties}
+        
 
 
-print(DbReadyQs.goals_for_each_team())
+
+for func in list(DbReadyQs.__dict__.keys())[4:-3]:
+    print("Function:",func, "\nOutput:", DbReadyQs.__dict__[func](), "\n")
