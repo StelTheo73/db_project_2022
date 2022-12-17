814af9ba7b73d341df6bbc1b968a2777e6ad4d20
Myron <gmyron1@gmail.com>
2022-12-17 20:36:13 +0200

    found a bag.. count 0 wins or 0 defeats. Count 0 ties solved

--- src/libraries/dbIO/DbReadyQs.py
+++ src/libraries/dbIO/DbReadyQs.py
@@ -8,7 +8,6 @@ class DbReadyQs:
 
     def format(cursor: object):
         "This function formats the outputs from the database for the GUI"
-        # return {entry[0]:entry[1] for entry in cursor}
         return dict(cursor)
 
     def matches_for_each_team():
@@ -44,10 +43,15 @@ class DbReadyQs:
         return DbReadyQs.format(cursor)
 
     def ties_for_each_team():
-        cursor = DbReadyQs.db.execute("SELECT team_name, count(match.match_id) FROM team, participation, match \
-            WHERE participation.match_id=match.match_id AND \
-            (team_name=home_team AND home_team_goals=away_team_goals) \
+        cursor = DbReadyQs.db.execute("SELECT team_name, count(match.match_id) FROM team \
+            JOIN participation ON (team_name=home_team OR team_name=away_team) \
+            LEFT JOIN match ON participation.match_id=match.match_id AND home_team_goals=away_team_goals \
             GROUP BY team_name ORDER BY count(match.match_id) DESC")
+        # Test for the case where wins=0 or defeats=0
+        # cursor = DbReadyQs.db.execute("SELECT team_name, count(match.match_id) FROM team \
+        #     LEFT JOIN participation ON ((team_name=home_team AND home_team_goals=away_team_goals) OR (team_name=away_team AND home_team_goals=away_team_goals)) \
+        #     LEFT JOIN match ON participation.match_id=match.match_id \
+        #     GROUP BY team_name ORDER BY count(match.match_id) DESC")
         return DbReadyQs.format(cursor)
 
     def defeats_for_each_team():
@@ -60,8 +64,8 @@ class DbReadyQs:
     def points_for_each_team():
         wins, ties = DbReadyQs.wins_for_each_team(), DbReadyQs.ties_for_each_team()
         points = lambda team: 3*wins[team] + 1*ties[team]
-        return {team:points(team) for team in ties}
-        
+        return DbReadyQs.format([(team,points(team)) for team in wins])
+
 
 
 
